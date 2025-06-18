4178b3bcc1f9f68210837021106741f87572d55a
ps08012002 <ps08012002@gmail.com>
2025-06-18 16:07:59 +0700

    penambahan beberapa program

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,30 @@
 import express from "express"
+import db from "./database.js"
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  try {
+    const test = await db.tb_kode.findMany()
+    res.send({"data": test})
+  } catch (error) {
+    res.status(500).send("internal server error")
+  }
+})
+
+app.post("/kode", async(req, res) => {
+  try {
+  console.log("req.body", req.body);
+  
+await db.tb_kode.create({data:{kode_tinta:req.body.kode_tinta}})
+res.send("sukses insert kode")
+  } catch (error) {
+    console.log("error", error);
+    
+     res.status(500).send({"internal server error": error})
+  }
 })
 
 app.listen(port, () => {
